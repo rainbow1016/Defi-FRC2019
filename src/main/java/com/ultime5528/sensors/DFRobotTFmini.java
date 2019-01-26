acfec1eb90daf5466ade72be36f8918985df7eb1
MingZhang1016 <prattkate33@gmail.com>
2019-01-26 21:24:00 +0100

    addition du Lidar TFmini

---
+++ src/main/java/com/ultime5528/sensors/DFRobotTFmini.java
@@ -0,0 +1,68 @@
+package com.ultime5528.sensors;
+
+import edu.wpi.first.wpilibj.Notifier;
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.SerialPort.Port;
+
+public class DFRobotTFmini {
+
+    private int distance;
+    private int strength;
+    private SerialPort serialPort;
+    private Notifier notifier;
+
+    public DFRobotTFmini() {
+        distance = 0;
+        strength = 0;
+        serialPort = new SerialPort(115200, Port.kOnboard);
+        notifier = new Notifier(()->measure());
+        notifier.startPeriodic(0.02);
+    }
+
+    private boolean measure() {
+
+        byte[] tableau = new byte[9];
+        long checksum = 0;
+
+        tableau[0] = serialPort.read(1)[0];
+        checksum += tableau[0];
+
+        if (tableau[0] == 'Y') {
+
+            tableau[1] = serialPort.read(1)[0];
+            checksum += tableau[1];
+
+            if (tableau[1] == 'Y') {
+
+                for (int i = 2; i < 8; i++) {
+                    tableau[i] = serialPort.read(1)[0];
+                    checksum += tableau[i];
+                }
+
+                tableau[8] = serialPort.read(1)[0];
+                checksum &= 0xff;
+
+                if (checksum == tableau[8]) {
+
+                    synchronized(this){
+                        distance = tableau[2] + tableau[3] * 256;
+                        strength = tableau[4] + tableau[5] * 256;
+                    }
+                    return true;
+
+                } 
+            }
+        }
+
+        return false;
+
+    }
+    public synchronized int getDistance(){
+        return distance;
+    }
+
+    public synchronized int getStrength() {
+        return strength;
+    }
+    
+}
