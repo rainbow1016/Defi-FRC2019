30e36277750966eb6506fd8d03c88c93c33ec01e
MingZhang1016 <prattkate33@gmail.com>
2019-01-26 16:18:50 -0500

    Gérer exception LIDAR

--- src/main/java/com/ultime5528/sensors/DFRobotTFmini.java
+++ src/main/java/com/ultime5528/sensors/DFRobotTFmini.java
@@ -2,7 +2,9 @@ package com.ultime5528.sensors;
 
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.SerialPort.Parity;
 import edu.wpi.first.wpilibj.SerialPort.Port;
+import edu.wpi.first.wpilibj.SerialPort.StopBits;
 
 public class DFRobotTFmini {
 
@@ -14,44 +16,61 @@ public class DFRobotTFmini {
     public DFRobotTFmini() {
         distance = 0;
         strength = 0;
-        serialPort = new SerialPort(115200, Port.kOnboard);
+        serialPort = new SerialPort(115200, Port.kOnboard, 8, Parity.kNone, StopBits.kOne);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        serialPort.reset();
+
         notifier = new Notifier(()->measure());
         notifier.startPeriodic(0.02);
     }
 
     private boolean measure() {
 
-        byte[] tableau = new byte[9];
-        long checksum = 0;
-
-        tableau[0] = serialPort.read(1)[0];
-        checksum += tableau[0];
-
-        if (tableau[0] == 'Y') {
-
-            tableau[1] = serialPort.read(1)[0];
-            checksum += tableau[1];
-
-            if (tableau[1] == 'Y') {
-
-                for (int i = 2; i < 8; i++) {
-                    tableau[i] = serialPort.read(1)[0];
-                    checksum += tableau[i];
-                }
-
-                tableau[8] = serialPort.read(1)[0];
-                checksum &= 0xff;
-
-                if (checksum == tableau[8]) {
-
-                    synchronized(this){
-                        distance = tableau[2] + tableau[3] * 256;
-                        strength = tableau[4] + tableau[5] * 256;
+        try {
+            
+            byte[] tableau = new byte[9];
+            long checksum = 0;
+    
+            tableau[0] = serialPort.read(1)[0];
+            checksum += tableau[0];
+    
+            if (tableau[0] == 'Y') {
+    
+                tableau[1] = serialPort.read(1)[0];
+                checksum += tableau[1];
+    
+                if (tableau[1] == 'Y') {
+    
+                    for (int i = 2; i < 8; i++) {
+                        tableau[i] = serialPort.read(1)[0];
+                        checksum += tableau[i];
                     }
-                    return true;
-
-                } 
+    
+                    tableau[8] = serialPort.read(1)[0];
+                    checksum &= 0xff;
+    
+                    if (checksum == tableau[8]) {
+    
+                        synchronized(this){
+                            distance = tableau[2] + tableau[3] * 256;
+                            strength = tableau[4] + tableau[5] * 256;
+                        }
+                        return true;
+    
+                    } 
+                }
             }
+    
+            return false;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
         return false;
