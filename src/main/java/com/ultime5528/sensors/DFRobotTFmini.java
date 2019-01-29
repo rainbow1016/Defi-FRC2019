d967ca23191efd5000f7ac89f66b97a9b982c00d
MingZhang1016 <prattkate33@gmail.com>
2019-01-29 16:54:07 -0500

    Lidar fonctionnel (YOUPI)

--- src/main/java/com/ultime5528/sensors/DFRobotTFmini.java
+++ src/main/java/com/ultime5528/sensors/DFRobotTFmini.java
@@ -16,7 +16,7 @@ public class DFRobotTFmini {
     public DFRobotTFmini() {
         distance = 0;
         strength = 0;
-        serialPort = new SerialPort(115200, Port.kOnboard, 8, Parity.kNone, StopBits.kOne);
+        serialPort = new SerialPort(115200, Port.kMXP, 8, Parity.kNone, StopBits.kOne);
 
         try {
             Thread.sleep(1000);
@@ -27,33 +27,35 @@ public class DFRobotTFmini {
 
         serialPort.reset();
 
-        notifier = new Notifier(()->measure());
-        notifier.startPeriodic(0.02);
+        notifier = new Notifier(()->{
+            while(true)measure();}
+        );
+        notifier.startSingle(0);
     }
 
     private boolean measure() {
-
+        
         try {
             
-            byte[] tableau = new byte[9];
+            int[] tableau = new int[9];
             long checksum = 0;
     
-            tableau[0] = serialPort.read(1)[0];
+            tableau[0] = read();
             checksum += tableau[0];
     
             if (tableau[0] == 'Y') {
     
-                tableau[1] = serialPort.read(1)[0];
+                tableau[1] = read();
                 checksum += tableau[1];
     
                 if (tableau[1] == 'Y') {
     
                     for (int i = 2; i < 8; i++) {
-                        tableau[i] = serialPort.read(1)[0];
+                        tableau[i] = read();
                         checksum += tableau[i];
                     }
     
-                    tableau[8] = serialPort.read(1)[0];
+                    tableau[8] = read();
                     checksum &= 0xff;
     
                     if (checksum == tableau[8]) {
@@ -63,7 +65,7 @@ public class DFRobotTFmini {
                             strength = tableau[4] + tableau[5] * 256;
                         }
                         return true;
-    
+                        
                     } 
                 }
             }
@@ -76,6 +78,23 @@ public class DFRobotTFmini {
         return false;
 
     }
+
+    private int read(){
+        int out = toUnsignedByte(serialPort.read(1)[0]);
+        if(out != 89) System.out.println(out); 
+        else System.out.println("Y");
+        return out;
+    }
+
+    private int toUnsignedByte(byte b){
+        int result;
+
+        if(b < 0) result= 256 + b;
+        else result = b;
+
+        return result;
+    }
+
     public synchronized int getDistance(){
         return distance;
     }
