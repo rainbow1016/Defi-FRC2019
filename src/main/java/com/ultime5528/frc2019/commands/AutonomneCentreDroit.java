8593114d163e91ff1360efda5ac8f347ef14b464
MingZhang1016 <prattkate33@gmail.com>
2019-02-09 14:20:27 -0500

    Auto stash before merge of "master" and "origin/master"

---
+++ src/main/java/com/ultime5528/frc2019/commands/AutonomneCentreDroit.java
@@ -0,0 +1,20 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package com.ultime5528.frc2019.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class AutonomneCentreDroit extends CommandGroup {
+  /**
+   * Add your docs here.
+   */
+  public AutonomneCentreDroit() {
+    addSequential();
+    addSequential(new DeposerHatch());
+  }
+}
