5a478e983337fc8721f721cb7ca6bc8e8c0e771a
MingZhang1016 <prattkate33@gmail.com>
2019-02-12 17:53:46 -0500

    commande conditionnel deposerHatch et commandes deposerhatchHaut/Bas

---
+++ src/main/java/com/ultime5528/frc2019/commands/DeposerHatchBas.java
@@ -0,0 +1,54 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package com.ultime5528.frc2019.commands;
+
+import com.ultime5528.frc2019.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class DeposerHatchBas extends Command {
+  public DeposerHatchBas() {
+    setTimeout(1.5);
+    requires(Robot.yntake);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    if (timeSinceInitialized() <= 1) {
+      Robot.yntake.pousserHaut();
+      Robot.yntake.pousserBas();
+    } else {
+      Robot.yntake.revenir();
+    }
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return isTimedOut();
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+    Robot.yntake.fermer();
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+    end();
+  }
+}
